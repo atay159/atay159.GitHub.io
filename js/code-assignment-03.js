265a985dd8f12f4b027b79f82ede1ab583213665
atay159 <35343457+atay159@users.noreply.github.com>
2018-02-15 10:35:38 -0600

    update

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,4 +1,11 @@
 let myNumber = 7
 let myString = 'Some Text'
 let myBoolean = true
-let myArray - ['apple', 'orange', 'pear']
+let myArray = ['apple', 'orange', 'pear']
+
+console.log('ID: ' + projectId);
+console.log('URL: ' + projectUrL);
+console.log('Title: ' + projectTitle);
+console.log('Decription: ' + projectDesc);
+console.log('Has thumbnail?' + projectThumb);
+console.log('Map extent: ' + projectMapExtent);
