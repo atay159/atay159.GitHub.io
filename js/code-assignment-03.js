cb8d0e861b00dc745a81cbd5853e1fcd712abed0
atay159 <35343457+atay159@users.noreply.github.com>
2018-02-16 21:18:47 -0600

    update

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -1,18 +1,19 @@
+// assign values to a set of variables representing a project's metadata
 let projectId = 1
-let projectUrL = 'https://arcg.is/1HnCr1'
+let projectUrl = 'https://arcg.is/1HnCr1'
 let projectTitle = 'Map of Recent Earthquakes'
-let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitutde 4.5 or greater'
+let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of magnitude 4.5 or greater that have occurred in the last 7 days. The dataset is streamed from the USGS.'
 let projectThumb = true
 let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 
-let projectArray = [
+let projectArray = {
   1,
   'https://arcg.is/1HnCr1',
   'Map of Recent Earthquakes'
   'An ArcGIS Online Web App showing worldwide earthquakes of magnitutde 4.5 or greater',
   true,
   ['earthquake', 'usgs']
-]
+}
 
 let projectObject = {
   id: 1,
@@ -28,9 +29,10 @@ let myString = 'Some Text'
 let myBoolean = true
 let myArray = ['apple', 'orange', 'pear']
 
-console.log('ID: ' + projectId);
-console.log('URL: ' + projectUrL);
-console.log('Title: ' + projectTitle);
-console.log('Decription: ' + projectDesc);
-console.log('Has thumbnail?' + projectThumb);
-console.log('Map extent: ' + projectMapExtent);
+// display messages in the browser's developer console, combining the values with text
+console.log('ID: ' + projectId)
+console.log('URL: ' + projectUrl)
+console.log('Title: ' + projectTitle)
+console.log('Description: ' + projectDesc)
+console.log('Has thumbnail? ' + projectThumb)
+console.log('Map extent: ' + projectMapExtent)
