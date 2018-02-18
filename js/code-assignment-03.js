b94d44330dac661e8bf166c9a00dcddd189219c1
atay159 <35343457+atay159@users.noreply.github.com>
2018-02-17 22:25:12 -0600

    update

--- js/code-assignment-03.js
+++ js/code-assignment-03.js
@@ -6,24 +6,6 @@ let projectDesc = 'An ArcGIS Online Web App showing worldwide earthquakes of mag
 let projectThumb = true
 let projectMapExtent = [48.51, -28.37, 2.63, -132.34]
 
-let projectArray = {
-  1,
-  'https://arcg.is/1HnCr1',
-  'Map of Recent Earthquakes'
-  'An ArcGIS Online Web App showing worldwide earthquakes of magnitutde 4.5 or greater',
-  true,
-  ['earthquake', 'usgs']
-}
-
-let projectObject = {
-  id: 1,
-  url: 'https://arcg.is/1HnCr1'
-  title: 'Map of Recent Earthquakes',
-  description: ''An ArcGIS Online Web App showing worldwide earthquakes of magnitutde 4.5 or greater'',
-  hasThumbnail: true,
-  keyword: ['earthqukes', 'usgs']
-}
-
 let myNumber = 7
 let myString = 'Some Text'
 let myBoolean = true
---
+++ js/code-assignment-04.js
@@ -0,0 +1,45 @@
+let projectArray = [
+  1,
+  'https://arcg.is/1HnCr1',
+  'Map of Recent Earthquakes'
+  'An ArcGIS Online Web App showing worldwide earthquakes of magnitutde 4.5 or greater',
+  true,
+  ['earthquake', 'usgs']
+]
+
+let projectObject = {
+  id: 1,
+  url: 'https://arcg.is/1HnCr1'
+  title: 'Map of Recent Earthquakes',
+  description: ''An ArcGIS Online Web App showing worldwide earthquakes of magnitutde 4.5 or greater'',
+  hasThumbnail: true,
+  keyword: ['earthqukes', 'usgs']
+}
+
+// This is an array
+let projectArray = [
+  1,
+  'https://arcg.is/9yWSf',
+  'LSU Reported Incidents',
+  'This website is for anyone on the LSU campus to report crimes and accidents.',
+  true,
+  ['crimes', 'incidents', 'accidents']
+]
+
+// This is an object. Each value is paired with a property.
+let projectObject = {
+  id: 1,
+  url: 'https://arcg.is/9yWSf',
+  title: 'LSU Reported Incidents',
+  desc: 'This website is for anyone on the LSU campus to report crimes and accidents.',
+  thumb: true,
+  keywords: [
+    'crimes',
+    'incidents',
+    'accidents'
+  ]
+}
+
+console.log(projectArray[2])
+console.log(projectObject.title)
+console.log(projectObject["title"])
