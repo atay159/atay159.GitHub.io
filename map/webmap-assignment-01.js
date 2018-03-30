43e0c800ed04a41885365d43070582ab60cca273
atay159 <35343457+atay159@users.noreply.github.com>
2018-03-30 15:55:57 -0500

    webmap assignment 1

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,9 +1,19 @@
 //let someName = L.map('your-map-div-id').setView([yourLat, yourLon], yourZoom)
-let map1 = L.map('map1').setView([30.01, 31.23], 16)
+let map1 = L.map('map1').setView([30.01, 31.23], 11)
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}').addTo(map1)
-let thismarker = L.marker([30.0, 31.2]).addTo(map1)
+let thismarker = L.marker([29.975258, 31.137545]).addTo(map1)
 let thisshape = L.polygon([
-  [30.07, 31.21],
-  [29.97, 31.05],
-  [29.96, 31.28]
+  [29.986715, 31.141093],
+  [29.968574, 31.142809],
+  [29.968798, 31.122639],
+  [29.986715, 31.122296]
+]).addTo(map1);
+thisshape.bindPopup('Great Pyramids of Egypt')
+thismarker.bindPopup('Great Spinx of Giza')
+map1.on('click', function (event) {
+  console.log('You selected ' + event.latlng)
+})
+let thispolyline = L.polyline([
+  [29.974932, 31.137454],
+  [29.975842, 31.13269]
 ]).addTo(map1);
