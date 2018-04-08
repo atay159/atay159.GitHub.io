49e55c00d623be0d4cbd5fa889c3915afdcba51a
atay159 <35343457+atay159@users.noreply.github.com>
2018-04-08 11:29:05 -0500

    webmap assignment 4

---
+++ map/webmap-assignment-04.js
@@ -0,0 +1,25 @@
+let map2 = L.map('map2').setView([39.3, -102.6], 3)
+map2url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+L.tileLayer(map2url).addTo(map2)
+
+thisStyle = function (State_Name) {
+  let population = feature.properties.POPULATION
+  let stateColor = 'Red'
+  if (POPULATION > 5000000) {stateColor = 'Purple' }
+  let thisStyle = {
+    color: thisstateColor,
+    weight: 2,
+    fillOpacity: 0.3
+  }
+  return thisStyle
+}
+function thisPopup (state, statelayer) {
+    let name = feature.properties.STATE_NAME
+    let population = feature.properties.POPULATION
+    statelayer.bindPopup('Population ' + name + ': ' + population + '<br>Greater than: 5000000')
+  }
+let thisOptions = {
+  style: thisStyle,
+  onEachFeature: thisPopup
+}
+L.geoJSON(statedemographics51, thisOptions).addTo(map2)
